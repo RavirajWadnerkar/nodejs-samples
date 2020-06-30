7afea1a90e32d9b79fa8f0792d19f99b1fe8c6a6
Raviraj Wadnerkar <ravirajwadnerkar@gmail.com>
2020-07-01 00:26:21 +0530

    server using express
    made a server using expressjs

--- express-server/server.js
+++ express-server/server.js
@@ -1,5 +1,12 @@
+//Steps to setup a express project
+// 1. npm init
+// 2. npm install express --save
+
 const express = require('express')
 const app = express()
+const users = require('./routes/user')
+
+app.use('/api',users)
 
 app.get('/', function(req,res) {
     res.send('Hello world!')
@@ -8,3 +15,10 @@ app.get('/', function(req,res) {
 app.listen(3000, function(){
     console.log('listening on http://localhost:3000')
 })
+
+// backend implementation
+// 1. history about nodejs
+// 2. implemented a basic server using nodejs
+// 3. then moved to expressjs
+    // 3.1 created a basic server using expressjs
+    // 3.2 created routes/api's
